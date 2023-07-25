668ad8e770e7ffe79cdb790b7de93eeaafad102d
Gnlow <gnlow@protonmail.com>
2023-07-26 01:26:36 +0900

    style

--- web/src/lib/Editor.svelte
+++ web/src/lib/Editor.svelte
@@ -10,15 +10,31 @@
 
     let value = 
 `|
-  -
-    | Hello,
-    | Bye,
-  - world!
+  - <h1>Flark</h1>
+  - <a href="https://github.com/flark-lang/flark">GitHub</a>
+  - 😎
 |
-  | asdf
+  -
+    | flexbox-based
+    | tiny
+  - markup language!
+|
   |
-    - made by
-    - gnlow`
+    - |
+    - -
+  | makes
+  |
+    - vertical
+    - horizontal
+  | box!
+|
+  | This editor
+  |
+    - built with
+    -
+      | CodeMirror
+      | Svelte
+`
 </script>
 
 <style>
@@ -26,28 +42,56 @@
         display: flex;
         flex-direction: column;
         padding: .3rem;
-        background-color: rgba(0,0,0,10%);
+        background-color: rgba(0,0,50,10%);
         gap: .3rem;
+        width: 100%;
     }
     :global(.flark-hbox) {
         display: flex;
         flex-direction: row;
         padding: .3rem;
-        background-color: rgba(0,0,0,10%);
+        background-color: rgba(0,0,50,10%);
         gap: .3rem;
+        align-items: center;
     }
     :global(.flark-text) {
         padding: .3rem;
         border: solid white 1px;
+        width: 100%;
+        box-sizing: border-box;
+        text-align: center;
+    }
+
+    app {
+        display: flex;
+        flex-direction: row;
+        height: 100vh;
+        font-size: 32px;
+        justify-content: center;
+        flex-wrap: wrap;
+        align-content: flex-start;
+    }
+    editor, preview {
+        width: 100%;
+        max-width: 600px;
+    }
+    preview {
+        font-family: monospace;
     }
 </style>
 
-<CodeMirror
-    bind:value
-    lang={flark()}
-    extensions={[
-        syntaxHighlighting(myHighlightStyle)
-    ]}
-/>
+<app>
+    <editor>
+        <CodeMirror
+            bind:value
+            lang={flark()}
+            extensions={[
+                syntaxHighlighting(myHighlightStyle)
+            ]}
+        />
+    </editor>
+    <preview>
+        {@html toHtml(value)} 
+    </preview>
+</app>
 
-{@html toHtml(value)}
