b1becfc746c0dd10bbc876e35dd4df27bad7d820
Gnlow <gnlow@protonmail.com>
2023-08-22 04:17:01 +0900

    itworksssssss

--- language/src/toHtml.ts
+++ language/src/toHtml.ts
@@ -11,33 +11,42 @@ export function toHtml(text: string) {
     const visit =
         (node: SyntaxNodeRef): Expr => {
             if (node.name == "LogicBlock") {
-                const [head, ...lines] = node.node.getChildren("LogicLine")
+                const [head, ...lines] =
+                    node.node.getChildren("LogicLine")
+                        .reverse()
                 return lines
                     .reduce((acc: Expr, x): Expr => {
                         const opName = x.node.firstChild.type.name.toLowerCase() as "or"
-                        return {[opName]: [acc, visit(x.node.lastChild)]}
+                        return {[opName]: [visit(x.node.lastChild), acc]}
                     },
                     visit(head.lastChild)
                     )
+            }
+            if (node.name == "Identifier") {
+                return {ref: getText(node.node)}
             }
             if (node.name == "String") {
                 const text = getText(node.node)
                 return {literal: text.substring(1, text.length-1)}
             }
-            if (node.name == "Bin") {
+            if (node.name == "BinExpr") {
                 const opName = node.node.firstChild.nextSibling.type.name.toLowerCase() as "or"
                 return {[opName]: [
                     visit(node.node.firstChild),
                     visit(node.node.lastChild),
-                ] as [Expr, Expr]}
+                ]}
+            }
+            if (node.name == "DefExpr") {
+                return {def: [
+                    getText(node.node.firstChild),
+                    visit(node.node.lastChild),
+                ]}
             }
         }
-    console.log(
-        visit(tree.topNode.firstChild),
-        $(expand(visit(tree.topNode.firstChild)))
+    console.log(visit(tree.topNode.firstChild))
+    //return JSON.stringify(visit(tree.topNode.firstChild))
+    return $(expand({ref: "pat"})(visit(tree.topNode.firstChild)))
+        .map(x => x.literal)
         .take(10)
-    )
-    return $(expand(visit(tree.topNode.firstChild)))
-        .map(x => JSON.stringify(x))
-        .take(10)
+        .join("<br>")
 }
