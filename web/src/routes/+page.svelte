a2bba5ab9d4a4eaf6b99bea5f221bc288d3a5479
Gnlow <gnlow@protonmail.com>
2023-08-30 01:50:44 +0900

    add logo

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script>
     import Editor from "$lib/Editor.svelte"
+    import logo from "$lib/assets/zy-logo.svg"
 </script>
 
+<h1 class="hbox px(20) pt(20) gap(10) font(32) bold monospace">
+    <img src={logo} alt="Zy logo" class="h(1em)"/>
+    Zy
+</h1>
 <Editor/>
