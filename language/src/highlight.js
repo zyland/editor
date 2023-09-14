da994b91b1eaf47865673d9013a1f47f81d2ff14
Gnlow <gnlow@protonmail.com>
2023-09-14 23:15:54 +0900

    parse number

--- language/src/highlight.js
+++ language/src/highlight.js
@@ -4,16 +4,18 @@ export const highlight = styleTags({
     String: t.string,
     Number: t.number,
     Identifier: t.name,
-    "Add Sub Mul Div Or And Join Def": t.operator,
+    "Add Sub Mul Div Join Def": t.operator,
+	"Or And": t.logicOperator,
 })
 
 import { HighlightStyle, syntaxHighlighting } from "@codemirror/language"
 
 export const colors = [
     { tag: t.string, color: "#baf56a" },
-    { tag: t.number, color: "#ca6ca7" },
+    { tag: t.number, color: "#c1b8ff" },
     { tag: t.name, color: "#fcc01b" },
     { tag: t.operator, color: "#e76a0b" },
+    { tag: t.logicOperator, color: "#b38b89" },
 ]
 
 export const extensions = [
